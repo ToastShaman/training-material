22e3d4500315e7dd5393b7366e8b1e12ea8b0b31
Kevin <kevin.denver@gmail.com>
2015-07-24 12:01:20 +0100

    feat(jasmine and protractor): Live coding session for jasmine and protractor

---
+++ jasmin-and-protractor/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jasminAndProtractorApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the jasminAndProtractorApp
+ */
+angular.module('jasminAndProtractorApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ jasmin-and-protractor/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jasminAndProtractorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jasminAndProtractorApp
+ */
+angular.module('jasminAndProtractorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
